6cb893d30d75c4726a03b107b1a89911d87c8291
frashealey <fraser42a@gmail.com>
2024-01-08 14:54:00 +0000

    testing code done?

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import re
 from enum import Enum
 import tkinter as tk
 from tkinter import filedialog
+import numpy as np
 from natsort import natsorted
 from skimage import io
 import torch
@@ -32,6 +33,9 @@ class ModelMode(Enum):
     evaluate = 2
     test = 3
 
+# choose whether to train, evaluate, or test model
+model_mode = ModelMode.train
+
 # configures cuda (if available)
 if torch.cuda.is_available():
     device = torch.device("cuda")
@@ -39,9 +43,6 @@ if torch.cuda.is_available():
     torch.cuda.empty_cache()
 else:
     device = torch.device("cpu")
-
-# choose whether to train, evaluate, or test model
-model_mode = ModelMode.train
 
 # hyper-parameters
 batch_size = 16
@@ -58,16 +59,14 @@ train_dir = "./dataset/training/"
 test_dir = "./dataset/testing/"    # (expert c used)
 pretrain_dir = "./pretrains/"
 
-
 def backprop(scaler, optim, loss):
     # uses gradient scaler to backpropagate and update weights
     scaler.scale(loss).backward()
     scaler.step(optim)
     scaler.update()
 
-
 def trainer(gen, dis, train_loader, gen_optim, dis_optim, gen_scaler, dis_scaler, recon_loss, adv_loss):
-    # defines tqdm (for progress bar)
+    # defines tqdm (progress bar)
     looper = tqdm(train_loader)
 
     # sets each model to training mode
@@ -92,7 +91,8 @@ def trainer(gen, dis, train_loader, gen_optim, dis_optim, gen_scaler, dis_scaler
             # discriminator output for fake image
             # (using .detach() to allow using the same fake_img during generator training)
             dis_fake = dis(x, fake_img.detach())
-            # calculates total discriminator loss as average of losses of discriminator being correct and being incorrect
+            # calculates total discriminator loss as average of losses
+            # of discriminator being correct and being incorrect
             loss_dis = (adv_loss(dis_real, torch.ones_like(dis_real))) + (adv_loss(dis_fake, torch.zeros_like(dis_fake))) * 0.5
 
         # updates discriminator weights through backpropagation
@@ -115,9 +115,62 @@ def trainer(gen, dis, train_loader, gen_optim, dis_optim, gen_scaler, dis_scaler
     # returns generator and discriminator losses
     return loss_gen, loss_dis
 
-def evaluator(gen, image_path, transform, denorm):
-    fig, (ax1, ax2) = plt.subplots(1, 2)
+def tester(gen, test_loader, denorm):
     gen.eval()
+
+    # defines arrays to store PSNR and SSIM
+    # values for each channel
+    psnr_arr = []
+    ssim_arr = []
+    # defines tqdm looper (display loading bar)
+    looper = tqdm(test_loader)
+    # iterates for every image in expert's testing set
+    for i, (x, y) in enumerate(looper):
+        x = x.to(device)
+        y = y.to(device)
+
+        # generator creates fake image
+        with torch.no_grad():
+            fake_img = gen(x)
+
+        # transforming tensors to computable images
+        real_img = denorm(y).cpu()[0].permute(1, 2, 0).numpy()
+        fake_img = denorm(fake_img).cpu()[0].permute(1, 2, 0).numpy()
+
+        # splitting by channel, as PSNR and SSIM can only
+        # be calculated on a single channel
+        real1 = real_img[:, :, 0]
+        real2 = real_img[:, :, 1]
+        real3 = real_img[:, :, 2]
+        fake1 = fake_img[:, :, 0]
+        fake2 = fake_img[:, :, 1]
+        fake3 = fake_img[:, :, 2]
+        data_range1 = fake1.max() - fake1.min()
+        data_range2 = fake2.max() - fake2.min()
+        data_range3 = fake3.max() - fake3.min()
+
+        # calculates PSNR and SSIM for each channel
+        # (using skimage)
+        psnr1 = psnr(real1, fake1, data_range=data_range1)
+        psnr2 = psnr(real2, fake2, data_range=data_range2)
+        psnr3 = psnr(real3, fake3, data_range=data_range3)
+        ssim1 = ssim(real1, fake1, data_range=data_range1)
+        ssim2 = ssim(real2, fake2, data_range=data_range2)
+        ssim3 = ssim(real3, fake3, data_range=data_range3)
+        # calculates the mean average of the 3 channels
+        psnr_arr.append((psnr1 + psnr2 + psnr3) / 3)
+        ssim_arr.append((ssim1 + ssim2 + ssim3) / 3)
+
+    # calculates and outputs the mean averages of PSNR and SSIM
+    # for all images in expert's testing set
+    psnr_mean = np.mean(np.array(psnr_arr))
+    ssim_mean = np.mean(np.array(ssim_arr))
+
+    return psnr_mean, ssim_mean
+
+def evaluator(gen, image_path, transform, denorm):
+    gen.eval()
+    fig, (ax1, ax2) = plt.subplots(1, 2)
 
     # read and transform image
     # (adding the batch dimension artifically using 'unsqueeze')
@@ -202,7 +255,8 @@ if __name__ == "__main__":
                         pretrain_dir,
                         pretrains[-1]
                     ),
-                    map_location="cpu"      # prevents CUDA out of memory error when loading checkpoint
+                    # (prevents CUDA out of memory error when loading checkpoint)
+                    map_location="cpu"
                 )
                 gen.load_state_dict(gen_checkpoint["model_state_dict"])
                 gen_optim.load_state_dict(gen_checkpoint["optimizer_state_dict"])
@@ -213,7 +267,8 @@ if __name__ == "__main__":
                         pretrain_dir,
                         pretrains[-2]
                     ),
-                    map_location="cpu"      # prevents CUDA out of memory error when loading checkpoint
+                    # (prevents CUDA out of memory error when loading checkpoint)
+                    map_location="cpu"
                 )
                 dis.load_state_dict(dis_checkpoint["model_state_dict"])
                 dis_optim.load_state_dict(dis_checkpoint["optimizer_state_dict"])
@@ -224,7 +279,17 @@ if __name__ == "__main__":
         # iterates for every epoch in num_epochs
         for epoch in range(start_epoch, num_epochs):
             print(f"Epoch {epoch}")
-            loss_gen, loss_dis = trainer(gen, dis, train_loader, gen_optim, dis_optim, gen_scaler, dis_scaler, recon_loss, adv_loss)
+            loss_gen, loss_dis = trainer(
+                gen,
+                dis,
+                train_loader,
+                gen_optim,
+                dis_optim,
+                gen_scaler,
+                dis_scaler,
+                recon_loss,
+                adv_loss
+            )
             print(f"Gen. loss: {loss_gen}")
             print(f"Dis. loss: {loss_dis}")
 
@@ -242,6 +307,60 @@ if __name__ == "__main__":
                 "optimizer_state_dict": dis_optim.state_dict(),
                 "loss": loss_dis
             }, f"{pretrain_dir}/{epoch}_dis.pth")
+
+    # testing mode selected
+    elif model_mode == ModelMode.test:
+        # checks model weights exist
+        # (only need to check for generator)
+        if len(pretrains) >= 1 and "gen.pth" in pretrains[-1]:
+            # loads generator from saved state dictionaries
+            gen_checkpoint = torch.load(
+                os.path.join(
+                    pretrain_dir,
+                    pretrains[-1]
+                )
+            )
+            gen.load_state_dict(gen_checkpoint["model_state_dict"])
+            gen_optim.load_state_dict(gen_checkpoint["optimizer_state_dict"])
+
+            # defines dataset and dataloader
+            test_dataset = ExposureDataset(
+                os.path.join(test_dir, "INPUT_IMAGES/"),
+                os.path.join(test_dir, "GT_IMAGES/"),
+                transform_input=transforms.Compose([
+                    transforms.ToPILImage(),
+                    transforms.Resize(256),
+                    transforms.CenterCrop(256),
+                    transforms.ToTensor()
+                ]),
+                transform_truth=transforms.Compose([
+                    transforms.ToPILImage(),
+                    transforms.Resize(256),
+                    transforms.CenterCrop(256),
+                    transforms.ToTensor()
+                ])
+            )
+            test_loader = DataLoader(
+                test_dataset,
+                batch_size=1,
+                num_workers=num_workers,
+                shuffle=False,
+                pin_memory=True
+            )
+
+            # runs psnr and ssim tests
+            psnr_mean, ssim_mean = tester(
+                gen,
+                test_loader,
+                transforms.Compose([
+                ])
+            )
+
+            print(f"PSNR: {psnr_mean}")
+            print(f"SSIM: {ssim_mean}")
+
+        else:
+            print("No pretrained model found - please ensure pretrain_dir is assigned correctly")
 
     # evaluation mode selected
     elif model_mode == ModelMode.evaluate:
@@ -269,7 +388,7 @@ if __name__ == "__main__":
             )
             root.destroy()
 
-            # file is selected
+            # file selected
             if len(selected_image) > 0:
                 evaluator(
                     gen,
--- models.py
+++ models.py
@@ -1,47 +1,11 @@
 import torch
 from torch import nn
-from utils import DownConv, UpConv, ResBlock
+from utils import DownConv, UpConv, ResizeUpConv, ResBlock
 
 
 class Generator(nn.Module):
     def __init__(self, in_channels=3, features=32):
         super(Generator, self).__init__()
-
-        self.down0 = DownConv(in_channels, features, kernel_size=4, stride=2, padding=1, norm=False)
-        self.down1 = DownConv(features, features * 2, kernel_size=4, stride=2, padding=1)
-        self.down2 = DownConv(features * 2, features * 4, kernel_size=4, stride=2, padding=1)
-        self.down3 = DownConv(features * 4, features * 8, kernel_size=4, stride=2, padding=1)
-
-        self.res = ResBlock(features * 8, features * 8)
-
-        self.up0 = UpConv(features * 8, features * 4, kernel_size=4, stride=2, padding=1)
-        self.up1 = UpConv(features * 4 * 2, features * 2, kernel_size=4, stride=2, padding=1)
-        self.up2 = UpConv(features * 2 * 2, features, kernel_size=4, stride=2, padding=1)
-        self.up3 = UpConv(features, in_channels, kernel_size=4, stride=2, padding=1, norm=False, activation=False)
-
-    def forward(self, x):
-        down0 = self.down0(x)
-        down1 = self.down1(down0)
-        down2 = self.down2(down1)
-        down3 = self.down3(down2)
-
-        res = down3.clone()
-        for i in range(0, 3):
-            res = self.res(res)
-
-        up0 = self.up0(res)
-        up1 = self.up1(torch.cat([up0, down2], 1))
-        up2 = self.up2(torch.cat([up1, down1], 1))
-        up3 = self.up3(up2)
-
-        # use tanh when data is [-1, 1]
-        # and sigmoid when data is [0, 1]
-        return nn.Sigmoid()(up3)
-
-
-class GlobalGenerator(nn.Module):
-    def __init__(self, in_channels=3, features=32):
-        super(GlobalGenerator, self).__init__()
 
         self.down0 = DownConv(in_channels, features, kernel_size=7, stride=1, padding=3, batch=False)
         self.down1 = DownConv(features, features * 2, kernel_size=3, stride=2, padding=1, batch=False)
@@ -50,9 +14,9 @@ class GlobalGenerator(nn.Module):
 
         self.res = ResBlock(features * 8, features * 8, batch=False)
 
-        self.up0 = UpConv(features * 8, features * 4, kernel_size=3, stride=2, padding=1, output_padding=1, batch=False)
-        self.up1 = UpConv(features * 4 * 2, features * 2, kernel_size=3, stride=2, padding=1, output_padding=1, batch=False)
-        self.up2 = UpConv(features * 2 * 2, features, kernel_size=3, stride=2, padding=1, output_padding=1, batch=False)
+        self.up0 = ResizeUpConv(features * 8, features * 4, kernel_size=3, stride=1, padding=1, batch=False)
+        self.up1 = ResizeUpConv(features * 4 * 2, features * 2, kernel_size=3, stride=1, padding=1, batch=False)
+        self.up2 = ResizeUpConv(features * 2 * 2, features, kernel_size=3, stride=1, padding=1, batch=False)
         self.up3 = DownConv(features, in_channels, kernel_size=7, stride=1, padding=3, norm=False, batch=False, activation=False)
 
     def forward(self, x):
@@ -73,52 +37,6 @@ class GlobalGenerator(nn.Module):
         # use tanh when data is [-1, 1]
         # and sigmoid when data is [0, 1]
         return nn.Sigmoid()(up3)
-
-
-class VanillaGenerator(nn.Module):
-    def __init__(self, in_channels=3, features=32):
-        super(VanillaGenerator, self).__init__()
-
-        self.down0 = DownConv(in_channels, features, kernel_size=4, stride=2, padding=1, norm=False)
-        self.down1 = DownConv(features, features * 2, kernel_size=4, stride=2, padding=1)
-        self.down2 = DownConv(features * 2, features * 4, kernel_size=4, stride=2, padding=1)
-        self.down3 = DownConv(features * 4, features * 8, kernel_size=4, stride=2, padding=1)
-        self.down4 = DownConv(features * 8, features * 8, kernel_size=4, stride=2, padding=1)
-        self.down5 = DownConv(features * 8, features * 8, kernel_size=4, stride=2, padding=1)
-        self.down6 = DownConv(features * 8, features * 8, kernel_size=4, stride=2, padding=1)
-        self.middle = DownConv(features * 8, features * 8, kernel_size=4, stride=2, padding=1, norm=False, activation=True, leaky=False)
-
-        self.up0 = UpConv(features * 8, features * 8, kernel_size=4, stride=2, padding=1, dropout=True)
-        self.up1 = UpConv(features * 8 * 2, features * 8, kernel_size=4, stride=2, padding=1, dropout=True)
-        self.up2 = UpConv(features * 8 * 2, features * 8, kernel_size=4, stride=2, padding=1, dropout=True)
-        self.up3 = UpConv(features * 8 * 2, features * 8, kernel_size=4, stride=2, padding=1)
-        self.up4 = UpConv(features * 8 * 2, features * 4, kernel_size=4, stride=2, padding=1)
-        self.up5 = UpConv(features * 4 * 2, features * 2, kernel_size=4, stride=2, padding=1)
-        self.up6 = UpConv(features * 2 * 2, features, kernel_size=4, stride=2, padding=1)
-        self.up7 = UpConv(features * 2, in_channels, kernel_size=4, stride=2, padding=1, norm=False, activation=False)
-
-    def forward(self, x):
-        down0 = self.down0(x)
-        down1 = self.down1(down0)
-        down2 = self.down2(down1)
-        down3 = self.down3(down2)
-        down4 = self.down4(down3)
-        down5 = self.down5(down4)
-        down6 = self.down6(down5)
-        middle = self.middle(down6)
-
-        up0 = self.up0(middle)
-        up1 = self.up1(torch.cat([up0, down6], 1))
-        up2 = self.up2(torch.cat([up1, down5], 1))
-        up3 = self.up3(torch.cat([up2, down4], 1))
-        up4 = self.up4(torch.cat([up3, down3], 1))
-        up5 = self.up5(torch.cat([up4, down2], 1))
-        up6 = self.up6(torch.cat([up5, down1], 1))
-        up7 = self.up7(torch.cat([up6, down0], 1))
-
-        # use tanh when data is [-1, 1]
-        # and sigmoid when data is [0, 1]
-        return nn.Sigmoid()(up7)
 
 
 class Discriminator(nn.Module):
